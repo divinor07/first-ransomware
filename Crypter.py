732ebd99576dea71e2a3da40c7767f457a9ea3e5
divinor07 <divino.sr@hotmail.com>
2020-04-08 01:15:17 -0300

    Criando o módulo Crypter

---
+++ Crypter.py
@@ -0,0 +1,19 @@
+# Método responsavel por encriptar e desencriptar os arquivos
+
+def change_files(filename, crytoFn, block_size=16):
+
+    with open(filename, 'r+b') as _file:
+        raw_value = _file.read(block_size)
+        while raw_value:
+            cipher_value = crytoFn(raw_value)
+            
+            # Compara o tamanho do bloco cifrado e plano (plain text)
+            if len(raw_value) != len(cipher_value):
+                raise ValueError('O valor cifrado {} tem um tamanho diferente do valor plano {}'.format(len(cipher_value), len(raw_value)))
+
+
+            _file.seek(-len(raw_value), 1)
+            _file.write(cipher_value)
+            raw_value = _file.read(block_size)
+            
+            
